cabadbe017fc0111415fe2fb2786998dc7749b9a
Sophia Aredas <sna49@cbsuschmidt.biohpc.cornell.edu>
2023-09-29 12:22:03 -0400

    Added file that loops over the 346 MAGs and annonates them. They will be placed into an output folder called genomad_output346 genomad_db >> results.out. the results.out is a file that shows the status of the file being executed and annontation.

--- Code/geNomad_pipeline_346MAGs.sh
+++ Code/geNomad_pipeline_346MAGs.sh
@@ -2,12 +2,17 @@
 source /programs/miniconda3/bin/activate genomad
 genomad download-database .
 
-#make sure that you are in the correct path /local/workdir/sna49/SA_Fall2023_Phage_Rotation/Data/346_MAGs/Output
+pwd #make sure that you are in the correct path /local/workdir/sna49/SA_Fall2023_Phage_Rotation/Data/346_MAGs/Output/
+
+#now time to run the loop to read the files and annonate them!
 for file in *.fa.gz; 
   do 
   # Which file am I on? 
   echo $file 
   # input comments here 
-  genomad end-to-end --cleanup --splits 32 $file genomad_output genomad_db >>results.out; 
+  genomad end-to-end --cleanup --splits 32 $file genomad_output_346 genomad_db >>results.out; 
   done
+  
+#the files that will be output will be within genomad_output_346. I will need to write a script later on that will combine all corresponding summary files (like phage genes or something) that way we can analyze the data
+  
   
